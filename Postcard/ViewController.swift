bfeb5adde64c1b8df75e617cfdd93a9fead990e6
michael <mk@michael-king.com>
2014-09-13 01:35:12 -0500

    initial build setting up project skeleton

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false;
+        messageLabel.textColor = UIColor.blueColor()
+        messageLabel.text = "sent a message to \(enterNameTextField.text).";
+        enterNameTextField.text = ""
+        enterMessageTextField.text = ""
+        enterNameTextField.hidden = true
+        enterMessageTextField.hidden = true
+        enterMessageTextField.resignFirstResponder(); //close the keyboard
+        mailButton.hidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
